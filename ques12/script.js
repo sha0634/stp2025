96209d82f0331a5973652756f7a79c7e2af5f4d0
sha0634 <akanksha7343@gmail.com>
2025-07-01 23:56:35 +0530

    ques12

---
+++ ques12/script.js
@@ -0,0 +1,15 @@
+// function person(n,a){
+//     this.name=n,
+//     this.age=a
+// }
+
+// person1=new person("person1",35);
+// console.log(person1)
+class person{
+    constructor(n,a){
+        this.name=n;
+        this.age=a;
+    }
+}
+ person1=new person("person1",35);
+ console.log(person1)
